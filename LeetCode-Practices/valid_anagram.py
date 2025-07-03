dfb702d31fe9d9e9d012383f1999614b25872598
Mahdiar Khodabakhshi <mahdiar.khodabakhshi@torontomu.ca>
2025-07-03 12:00:00 -0400

    Valid String Anagram

--- LeetCode-Practices/valid_anagram.py
+++ LeetCode-Practices/valid_anagram.py
@@ -14,8 +14,39 @@ class BruteForce:
             
         return True
     
+class MergeSort:
+    def merge_sort(self, arr):
+        if len(arr) <= 1:
+            return arr
+
+        mid   = len(arr) // 2
+        left  = self.merge_sort(arr[:mid])
+        right = self.merge_sort(arr[mid:])
+
+        return self._merge(left, right)
+
+    def _merge(self, left, right):
+        result, i, j = [], 0, 0
+        while i < len(left) and j < len(right):
+            if left[i] <= right[j]:
+                result.append(left[i]);  i += 1
+            else:
+                result.append(right[j]); j += 1
+        result.extend(left[i:])
+        result.extend(right[j:])
+        return result
+
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        return self.merge_sort(list(s)) == self.merge_sort(list(t))
+ 
 
 test = BruteForce()
+merge_sort_test = MergeSort()
+
+print(merge_sort_test.isAnagram("mahdiar", "mahdiar"))
+print(merge_sort_test.isAnagram("mahdiyar", "mahdiar"))
 
 print(test.brute_force("mahdiar", "mahdiyar"))
 print(test.brute_force("mahdiar", "mahdiar"))
